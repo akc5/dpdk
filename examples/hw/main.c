53840689b28fc8bc5a12e4cea51cb74de30e485a
Abhishek Choubey <mail.akchoubey@gmail.com>
2023-08-24 14:21:12 +0530

    do an rte_eal_init in the hw application

--- examples/hw/main.c
+++ examples/hw/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <rte_eal.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
+	int ret = rte_eal_init(argc, argv);
+	if (ret < 0)
+	{
+		printf("rte_eal_init: failed\n");
+		return 1;
+	}
 	printf("Hello World !\n");
 	return 0;
 }
